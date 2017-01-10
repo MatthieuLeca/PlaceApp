4af8eb7e41e0c3bcf27458080db105ad013a7d40
MatthieuL <leca.matthieu@gmail.com>
2017-01-10 22:39:24 +0100

    First push. App fonctionnelle de l'API place

---
+++ app/src/main/java/com/matt/mentor/MainActivity.java
@@ -0,0 +1,86 @@
+package com.matt.mentor;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+import static java.security.AccessController.getContext;
+
+
+public class MainActivity extends AppCompatActivity {
+    TextView text1 = null;
+    Button searchBut = null;
+    int status;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text1 = (TextView) findViewById(R.id.textView1);
+        searchBut = (Button) findViewById(R.id.buttonSearch);
+
+        status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (status != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(status)) {
+                GooglePlayServicesUtil.getErrorDialog(status, this,
+                        100).show();
+            }
+        }
+
+        searchBut.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                launchAPI();
+            }
+        });
+
+
+    }
+    private void launchAPI(){
+        if (status == ConnectionResult.SUCCESS) {
+            int PLACE_PICKER_REQUEST = 199;
+            PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+            try {
+                startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+            } catch (GooglePlayServicesRepairableException e) {
+                e.printStackTrace();
+            } catch (GooglePlayServicesNotAvailableException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100){
+            status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        }
+        if (requestCode == 199){
+
+            //process Intent......
+            Place place = PlacePicker.getPlace(data, this);
+            String toastMsg = String.format("Place: %s", place.getName());
+            text1.setText(toastMsg);
+        }
+    }
+
+
+
+}
